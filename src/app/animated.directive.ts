d8366ba2a46f146802c53b20ed72ae1828166df2
Jacob Janisz <jacob.janisz@outlook.com>
2020-04-17 15:29:56 +0100

    gsap-only

--- src/app/animated.directive.ts
+++ src/app/animated.directive.ts
@@ -1,15 +1,12 @@
 import { Directive, ElementRef, EventEmitter, Input, OnInit, OnDestroy, Output } from '@angular/core';
-import {
-  animation, trigger, animateChild, group,
-  transition, animate, style, query, AnimationBuilder
-} from '@angular/animations';
+
 
 @Directive({
   selector: '[animated]'
 })
 export class AnimatedDirective {
 
-  constructor(private element: ElementRef, private _builder: AnimationBuilder) { }
+  constructor(private element: ElementRef) { }
 
   ngOnInit(){
 
@@ -21,20 +18,6 @@ export class AnimatedDirective {
       threshold: 0
     };
 
-    const myAnimation = this._builder.build([
-      style({ opacity: 0 }),
-      animate(500, style({ opacity: 1 }))
-    ]);
-
-    // use the returned factory object to create a player
-    const player = myAnimation.create(el);
-    player.play();
-
-    //player.setPosition(0);
-    //setTimeout(function(){
-      console.log(player.totalTime);
-    //}, 3000);
-
     el.observer = new IntersectionObserver(entry => {
       if (entry[0].intersectionRatio > 0) {
           
@@ -42,26 +25,6 @@ export class AnimatedDirective {
           
       }
     }, observerOptions);
-  }
-
-    setPosition(p: number): void {
-    const timeAtPosition = p * this.totalTime;
-    this.players.forEach(player => {
-      const position = player.totalTime ? Math.min(1, timeAtPosition / player.totalTime) : 1;
-      player.setPosition(position);
-    });
-  }
-
-  getPosition(): number {
-    const longestPlayer = this.players.reduce((longestSoFar, player) => {
-      const newPlayerIsLongest = player.totalTime > (longestSoFar !as AnimationPlayer).totalTime;
-      if (longestSoFar === undefined || newPlayerIsLongest) {
-        return player;
-      } else {
-        return longestSoFar;
-      }
-    }, undefined);
-    return longestPlayer !== undefined ? longestPlayer.getPosition() / longestPlayer.totalTime : 0;
   }
 
 }
